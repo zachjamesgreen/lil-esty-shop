1a13358593d57ffc6ef01cef67803162eb49cce5
NoahZinter <noah.zinter@gmail.com>
2021-06-02 20:12:04 -0600

    Test top five, create top_five instance method in Merchant class

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,12 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def top_five
+    a = Item.joins(invoices: :transactions)
+    .group(:id)
+    .where('transactions.result = 2')
+    .select("items.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
+    .order(revenue: :desc)
+    .limit(5)
+  end
 end
