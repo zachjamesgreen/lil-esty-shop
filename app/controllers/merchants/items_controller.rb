cb5f0cc929d9f076a119b236f19f7cdca7574113
Alex Klick <alexmac@Alexs-MacBook-Air.local>
2021-06-03 12:29:51 -0600

    added navbar and merchant items to ship user story

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -2,4 +2,7 @@ class Merchants::ItemsController < ApplicationController
   def index
     
   end
+  def show
+    @item = Item.find(params[:id])
+  end
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -5,8 +5,9 @@ class InvoiceItem < ApplicationRecord
   enum status: {pending: 0, packaged: 1, shipped: 2}
   # enum status: [:pending, :packaged, :shipped]
   def self.merch_not_shipped(merch_id)
-    select('invoice_items.*, merchants.id as merch_id')
+    select('invoice_items.*, merchants.id as merch_id, items.name as name, invoices.id as invoice_id')
     .joins(item: :merchant)
+    .joins(:invoice)
     .where.not(status: 2)
     .where('merchants.id = ?', merch_id)
   end
