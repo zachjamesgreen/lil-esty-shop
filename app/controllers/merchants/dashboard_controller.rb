56f897c7cd74e01e3354648fee2c38725f0b2b22
Alex Klick <alexmac@Alexs-MacBook-Air.local>
2021-06-03 10:37:29 -0600

    merch dashboard stats

--- app/controllers/merchants/dashboard_controller.rb
+++ app/controllers/merchants/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class Merchants::DashboardController < ApplicationController
   def index
     @merchant = Merchant.find(params[:id])
+    @customers = Customer.select('customers.*, merchant_id, count(transactions) as trans_count, transactions.result')
+    .joins(invoices: [{invoice_items: {item: :merchant}},:transactions])
+    .group(:merchant_id, :id, 'transactions.result')
+    .order(trans_count: :desc)
+    .where('merchant_id = 10')
+    .where('transactions.result = 1')
   end
 end
