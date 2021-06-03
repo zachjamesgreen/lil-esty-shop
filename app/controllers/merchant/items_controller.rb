15132259df2793249a2987ed134931e7f3dc9896
Zach T <ztrokey@gmail.com>
2021-06-02 21:35:02 -0600

    add merchant item show page

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -3,4 +3,8 @@ class Merchant::ItemsController < ApplicationController
     @merchant = Merchant.find(params[:id])
     @items = Item.where("merchant_id = #{@merchant.id}")
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   namespace :merchant do
     get '/:id/dashboard', to: 'dashboard#index'
+    get '/:id/items/:id', to: 'items#show'
     get '/:id/items', to: 'items#index'
     get '/:id/invoices', to: 'invoices#index'
   end
