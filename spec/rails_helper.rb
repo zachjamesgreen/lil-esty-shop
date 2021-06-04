0fad806dec441bfa027aee77be9e1954f49c2384
NoahZinter <noah.zinter@gmail.com>
2021-06-04 15:20:07 -0600

    refactor manufactuer items update to play nice with status update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,8 +43,8 @@ Shoulda::Matchers.configure do |config|
 end
 
 # Capybara.default_driver = :selenium
-# Capybara.default_driver = :selenium_chrome_headless
-Capybara.default_driver = :selenium_headless
+Capybara.default_driver = :selenium_chrome_headless
+# Capybara.default_driver = :selenium_headless
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
