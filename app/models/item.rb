5e366bfdf882c24a8962c46abd632c0c738da9fe
NoahZinter <noah.zinter@gmail.com>
2021-06-07 12:21:51 -0600

    Refactor top_day method in item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,6 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
-
   has_many :transactions, through: :invoices
 
   enum status: {disabled: 0, enabled:1}
@@ -10,19 +9,14 @@ class Item < ApplicationRecord
   validates :name, :description, :unit_price, presence: true
 
   def top_day
-    limit = invoices.joins(:invoice_items).where(status: 2).count
-    if limit >= 1
-      date_string = invoices.joins(:invoice_items)
+      date_string = invoices.joins(:invoice_items, :transactions)
+      .where("transactions.result = 2")
       .select("invoices.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
-      .where(status: 2)
       .group(:id)
       .order(revenue: :desc, created_at: :desc)
       .limit(1)
       .first
       .created_at
       date_string.to_date
-    else
-      "Transactions Successful, but Invoices Pending/Cancelled"
-    end
   end
 end
