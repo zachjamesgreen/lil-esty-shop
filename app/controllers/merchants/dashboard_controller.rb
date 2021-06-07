d11077f0894d4c8788a50ff60524c098fbc74f8f
Alex Klick <aklick12@gmail.com>
2021-06-07 17:10:26 -0600

    refactor controllers and models

--- app/controllers/merchants/dashboard_controller.rb
+++ app/controllers/merchants/dashboard_controller.rb
@@ -1,13 +1,8 @@
 class Merchants::DashboardController < ApplicationController
   def index
     @merchant = Merchant.find(params[:id])
-    @customers = Customer.select('customers.*, merchant_id, count(transactions) as trans_count, transactions.result')
-    .joins(invoices: [{invoice_items: {item: :merchant}},:transactions])
-    .group(:merchant_id, :id, 'transactions.result')
-    .order(trans_count: :desc)
-    .where('merchant_id = 10')
-    .where('transactions.result = 2')
-    .limit(5)
+    @customers = Customer.top_five(params[:id])
     @items_pending = InvoiceItem.merch_not_shipped(params[:id])
   end
+
 end
