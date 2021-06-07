092ad05363d06267442174a08d4f2cccf1ec8e7a
Zach Green <zachjamesgreen@gmail.com>
2021-06-07 13:30:49 -0600

    Can create a merchant from the admin-merchant index closes #15

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2021_06_05_163003) do
-
+ActiveRecord::Schema.define(version: 2021_06_07_165549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +59,7 @@ ActiveRecord::Schema.define(version: 2021_06_05_163003) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "enabled", default: true
+    t.boolean "enabled", default: false
   end
 
   create_table "transactions", force: :cascade do |t|
