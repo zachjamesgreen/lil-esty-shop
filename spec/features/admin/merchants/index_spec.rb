9935f0f4ef44a390195ebc988350b0c199614c60
Zach Green <zachjamesgreen@gmail.com>
2021-06-07 10:48:50 -0600

    admin merchant index shows merchants by status closes #14

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -4,16 +4,31 @@ RSpec.describe 'Admin Merchant Index' do
   it 'shows all merchants' do
     count = Merchant.all.size
     visit '/admin/merchants'
-    expect(page.all('#merchants li').count).to eq count
+    expect(page.all('.merchants li').count).to eq count
   end
 
   it 'should have enable/disable button next to merchant name' do
     merchant = Merchant.all.order(:name).first
     visit '/admin/merchants'
-    within "#merchants #merchant-#{merchant.id}" do
+    within ".merchants #merchant-#{merchant.id}" do
       expect(page).to have_selector(:link_or_button, 'Enabled')
       click_button('Enabled')
       expect(page).to have_selector(:link_or_button, 'Disabled')
+    end
+  end
+
+  it 'shows merchants by status enabled/disabled' do
+    # Merchant.first.update(enabled: false)
+    enabled_count = Merchant.where(enabled: true).size
+    disabled_count = Merchant.where(enabled: false).size
+    visit '/admin/merchants'
+    within '#enabled' do
+      expect(page.find('h3')).to have_content('Enabled')
+      expect(page.all('li').size).to eq enabled_count
+    end
+    within '#disabled' do
+      expect(page.find('h3')).to have_content('Disabled')
+      expect(page.all('li').size).to eq disabled_count
     end
   end
 end
