30d743589d573fac01363492e3e8af357524d380
Alex Klick <aklick12@gmail.com>
2021-06-07 17:39:10 -0600

    fixed component style

--- app/javascript/components/Footer.jsx
+++ app/javascript/components/Footer.jsx
@@ -25,58 +25,56 @@ class Footer extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div className="col-6 col-sm-3 mt-2 pt-2">
-          <blockquote className="blockquote">
-            <div className="pl-2">
-              <h5 style={{ color: "rgb(240, 242, 245)" }} className="nav-link">
-                Alex Klick
-              </h5>
-            </div>
-            <hr
-              className="float-left"
-              style={{
-                color: "rgb(105, 0, 0)",
-                marginLeft: "15px",
-                border: "3px solid rgb(125, 0, 0)",
-                width: "90%",
-              }}
-            />
+        <blockquote className="blockquote">
+          <div className="pl-2">
+            <h5 style={{ color: "rgb(240, 242, 245)" }} className="nav-link">
+              Alex Klick
+            </h5>
+          </div>
+          <hr
+            className="float-left"
+            style={{
+              color: "rgb(105, 0, 0)",
+              marginLeft: "15px",
+              border: "3px solid rgb(125, 0, 0)",
+              width: "90%",
+            }}
+          />
+          <a
+            className="nav-link"
+            style={{ color: "rgb(240, 242, 245)" }}
+            href="#"
+            onMouseOver={() => this.changeBackground}
+          >
+            Connect
+          </a>
+          <a
+            href={this.state.github_data.html_url}
+            data-toggle="tooltip"
+            data-placement="top"
+            title="click to view"
+            className="nav-link"
+            style={{ color: "rgb(240, 242, 245)" }}
+          >
+            Github: {this.state.github_data.login}
+          </a>
+          <span
+            className="align-baseline"
+            style={{ color: "rgb(240, 242, 245)" }}
+          >
             <a
               className="nav-link"
               style={{ color: "rgb(240, 242, 245)" }}
-              href="#"
-              onMouseOver={() => this.changeBackground}
-            >
-              Connect
-            </a>
-            <a
-              href={this.state.github_data.html_url}
-              data-toggle="tooltip"
-              data-placement="top"
-              title="click to view"
-              className="nav-link"
-              style={{ color: "rgb(240, 242, 245)" }}
-            >
-              Github: {this.state.github_data.login}
-            </a>
-            <span
-              className="align-baseline"
-              style={{ color: "rgb(240, 242, 245)" }}
-            >
-              <a
-                className="nav-link"
-                style={{ color: "rgb(240, 242, 245)" }}
-                href="https://www.linkedin.com/in/alex-klick
+              href="https://www.linkedin.com/in/alex-klick
                     "
-              >
-                Linkedin
-              </a>
-              <a className="nav-link" style={{ color: "rgb(240, 242, 245)" }}>
-                aklick12@gmail.com
-              </a>
-            </span>
-          </blockquote>
-        </div>
+            >
+              Linkedin
+            </a>
+            <a className="nav-link" style={{ color: "rgb(240, 242, 245)" }}>
+              aklick12@gmail.com
+            </a>
+          </span>
+        </blockquote>
       </React.Fragment>
     );
   }
