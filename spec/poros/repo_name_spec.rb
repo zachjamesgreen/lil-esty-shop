653b173aff71921a8e2a94280d06c836d2357a3d
NoahZinter <noah.zinter@gmail.com>
2021-06-10 18:46:44 -0600

    Refactor repo name spec to remove duplicate before block

--- spec/poros/repo_name_spec.rb
+++ spec/poros/repo_name_spec.rb
@@ -1,23 +1,6 @@
 require 'rails_helper'
 
 describe RepoName do
-  # before :each do
-  #   @github_data = {
-  #     "id": 373926639,
-  #     "node_id": "MDEwOlJlcG9zaXRvcnkzNzM5MjY2Mzk=",
-  #     "name": "lil-esty-shop"
-  #     }
-  #   class_double('GithubService', :retrieve_name => @github_data).as_stubbed_const
-  # end
-
-  # before(:each) do
-  #   @github_data = {
-  #     "id": 373926639,
-  #     "node_id": "MDEwOlJlcG9zaXRvcnkzNzM5MjY2Mzk=",
-  #     "name": "lil-esty-shop"}
-
-  #   class_double('GithubService', :retrieve_name => @github_data).as_stubbed_const
-  # end
   it 'returns a hash with the repo information' do
     expect(RepoName.repo_name).to eq("bulk_discounts")
   end
